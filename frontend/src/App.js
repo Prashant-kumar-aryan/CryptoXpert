900f7960472be908e81c2096b3e4ce76df2f1671
Prashant-kumar-aryan <prashantkr2698@gmail.com>
2024-03-11 18:03:02 +0530

    bugs removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,12 @@ import CoinDetails from "./component/CoinDetails.jsx";
 import Contact from "./component/Contact";
 import Coins from "./component/Coins";
 import Exchanges from "./component/Exchanges";
-<<<<<<< HEAD
 import Footer from "./component/Footer.jsx";
 import Whatsapp from "./component/Whatsapp.jsx";
 import Chat from "./component/Chat/Chat.jsx";
 import Ai from "./component/gemini/Ai.jsx";
-=======
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
->>>>>>> f980ec7a824ce2a400c9ccce89eea50d45c655c7
 function App() {
   return (
     <Router>
@@ -27,13 +24,10 @@ function App() {
         <Route path="/exchanges" element={<Exchanges />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-<<<<<<< HEAD
         <Route path="/chat" element={<Chat />} />
         <Route path="/ai" element={<Ai />} />
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
->>>>>>> f980ec7a824ce2a400c9ccce89eea50d45c655c7
       </Routes>
       <Footer />
       <Whatsapp />
