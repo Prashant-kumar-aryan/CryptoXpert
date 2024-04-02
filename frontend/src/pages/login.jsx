ce0b46f7d48468cce010948d8ef5443f914fc9e7
Prashant-kumar-aryan <prashantkr2698@gmail.com>
2024-04-03 00:40:26 +0530

    slight changes by prashant

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
             />
             {error && <div className="error_msg">{error}</div>}
             <button type="submit" className="green_btn">
-              Sing In
+              Sign In
             </button>
           </form>
         </div>
@@ -67,7 +67,7 @@ const Login = () => {
           <h1>New Here ?</h1>
           <Link to="/signup">
             <button type="button" className="white_btn">
-              Sing Up
+              Sign Up
             </button>
           </Link>
         </div>
